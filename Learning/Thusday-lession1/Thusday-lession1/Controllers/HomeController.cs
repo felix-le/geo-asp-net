606dec163fe52fe0f3e2075b4a45f1d45d2d8e02
Felix Le <mc.vietanh@gmail.com>
2021-10-10 10:06:41 -0400

    working on class lecture

---
+++ Learning/Thusday-lession1/Thusday-lession1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Thusday_lession1.Models;
+
+namespace Thusday_lession1.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    // make a new method that will load at /Home/About
+    public IActionResult About()
+    {
+      /* IActionResult usually returns a view (a web page) but could also return: 
+       * Json object, HTTP status code (e.g. 404, 401..), file, redirect
+       */
+      return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
