7831d6d9ace7cf27ccdc9bc44a07c0a64daad243
Felix Le <mc.vietanh@gmail.com>
2021-10-11 09:30:14 -0400

    cartItem updated

---
+++ Learning/Thusday-lession1/Thusday-lession1/Models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thusday_lession1.Models
+{
+  public class CartItem
+  {
+    public int CartItemId { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public string CustomerId { get; set; }
+
+    [Required]
+    public int PetId { get; set; }
+  }
+}
