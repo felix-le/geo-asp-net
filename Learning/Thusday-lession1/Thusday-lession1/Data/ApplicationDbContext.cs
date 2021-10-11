6e5f53a587ce3fac2db7b4d9ad45775283b57724
Felix Le <mc.vietanh@gmail.com>
2021-10-11 11:17:15 -0400

    Connect Azure DB

--- Learning/Thusday-lession1/Thusday-lession1/Data/ApplicationDbContext.cs
+++ Learning/Thusday-lession1/Thusday-lession1/Data/ApplicationDbContext.cs
@@ -3,14 +3,25 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Thusday_lession1.Models;
 
 namespace Thusday_lession1.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+  public class ApplicationDbContext : IdentityDbContext
+  {
+
+    //Make global references to our Models for use with our DB connection
+    // These 5 objects will have built-in CRUD methods we can execute without using SQL
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        {
-        }
+    {
     }
+  }
 }
