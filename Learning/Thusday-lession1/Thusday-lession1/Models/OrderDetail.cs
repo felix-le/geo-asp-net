4c0dfab99d12de1967b1e175cdc89226d256f9f4
Felix Le <mc.vietanh@gmail.com>
2021-10-11 09:36:36 -0400

    Done Models

---
+++ Learning/Thusday-lession1/Thusday-lession1/Models/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thusday_lession1.Models
+{
+  public class OrderDetail
+  {
+    public int OrderDetailId { get; set; }
+
+    [Required]
+    public int Quantity { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public int OrderId { get; set; }
+
+    [Required]
+    public int PetId { get; set; }
+  }
+}
