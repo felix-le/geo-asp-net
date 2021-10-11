312e4aa7e41d93d9f389694c2ced71f4203292a4
Felix Le <mc.vietanh@gmail.com>
2021-10-11 16:30:36 -0400

    working on relationship

--- ASMT2/TodoList/TodoList/Data/ApplicationDbContext.cs
+++ ASMT2/TodoList/TodoList/Data/ApplicationDbContext.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using TodoList.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace TodoList.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+  public class ApplicationDbContext : IdentityDbContext
+  {
+
+    // make global references from model to D
+    public DbSet<TodoListContent> TodoLists { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
+  }
 }
 
