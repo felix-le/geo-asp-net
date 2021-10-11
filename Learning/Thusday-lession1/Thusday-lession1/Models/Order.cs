e180574ca8675d1d774c4259303811d68c376dd8
Felix Le <mc.vietanh@gmail.com>
2021-10-11 09:23:18 -0400

    First model

---
+++ Learning/Thusday-lession1/Thusday-lession1/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thusday_lession1.Models
+{
+  public class Order
+  {
+    public int OrderId
+    {
+      get; set;
+    }
+
+    public DateTime OrderDate { get; set; }
+
+    public decimal OrderTotal { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public string Address { get; set; }
+
+    [Required]
+    [MaxLength(2)]
+    public string Province { get; set; }
+
+    [Required]
+    public string CustomerId { get; set; }
+  }
+}
---
+++ Learning/Thusday-lession1/Thusday-lession1/Models/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thusday_lession1.Models
+{
+  public class Pet
+  {
+    public int PetId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+  }
+}
